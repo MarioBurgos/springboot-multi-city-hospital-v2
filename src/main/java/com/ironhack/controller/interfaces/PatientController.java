3dcb8f36e6eb1e14e652c6eeb778ec39c7aa05d6
Mario Burgos <mario.burpi@gmail.com>
2023-02-07 21:53:56 +0100

    move logic to services

--- src/main/java/com/ironhack/controller/interfaces/PatientController.java
+++ src/main/java/com/ironhack/controller/interfaces/PatientController.java
@@ -1,7 +1,14 @@
 package com.ironhack.controller.interfaces;
 
+import com.ironhack.dto.DoctorDepartmentDTO;
+import com.ironhack.dto.DoctorStatusDTO;
 import com.ironhack.enums.Status;
+import com.ironhack.model.Doctor;
 import com.ironhack.model.Patient;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Date;
 import java.util.List;
@@ -14,7 +21,6 @@ public interface PatientController {
     List<Patient> findByDateOfBirthBetween(String start, String end);
     List<Patient> findByDepartment(String department);
     List<Patient> findByDoctorStatusOFF();
-
-
-
+    void add(Patient patient);
+    void update(Long id, Patient patient);
 }
